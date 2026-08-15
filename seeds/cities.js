c221aa0baee8479ed7b9c967b8b6861465a0c0c9
Calista <nwannecalista101@gmail.com>
2026-08-15 11:44:42 +0100

    Add working seed script with basic single-campground test

---
+++ seeds/cities.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    city: "Onitsha",
+    growth_from_2000_to_2013: "12.5%",
+    latitude: 6.1498,
+    longitude: 6.7867,
+    population: 561066,
+    rank: 1,
+    state: "Anambra",
+  },
+  {
+    city: "Awka",
+    growth_from_2000_to_2013: "15.2%",
+    latitude: 6.2101,
+    longitude: 7.0741,
+    population: 301657,
+    rank: 2,
+    state: "Anambra",
+  },
+  {
+    city: "Enugu",
+    growth_from_2000_to_2013: "18.4%",
+    latitude: 6.4584,
+    longitude: 7.5464,
+    population: 722664,
+    rank: 3,
+    state: "Enugu",
+  },
+];
---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  const c = new Campground({ title: "purple field" });
+  await c.save();
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
