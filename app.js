7da2fa0a75003b7dcd3e67f889f5977e894df941
Calista <nwannecalista101@gmail.com>
2026-08-17 04:38:26 +0100

    Add Bootstrap and multiple campground images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -45,14 +47,14 @@ app.post("/campgrounds", async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/show", { campground });
-});
-
 app.get("/campgrounds/:id/edit", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit", { campground });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.put("/campgrounds/:id", async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
+const {
+  places,
+  descriptors,
+  buildDescription,
+  images,
+} = require("./seedHelpers");
 const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -13,14 +18,30 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+const sampleImages = (array) => {
+  const shuffled = [...array].sort(() => 0.5 - Math.random());
+  const numberOfImages = Math.floor(Math.random() * 3) + 2;
+
+  return shuffled.slice(0, numberOfImages);
+};
+
 const seedDB = async () => {
   await Campground.deleteMany({});
+
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      description: buildDescription(
+        cities[random1000].city,
+        cities[random1000].state,
+      ),
+      images: sampleImages(images),
+      price: (Math.floor(Math.random() * 96) + 5) * 500,
     });
+
     await camp.save();
   }
 };
